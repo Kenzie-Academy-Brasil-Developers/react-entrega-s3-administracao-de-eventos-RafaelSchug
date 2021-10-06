99a9d6096ce79d79e8b5a6f1cab59f80d3a832d3
Rafael Schug <rafaelschugbc@gmail.com>
2021-10-06 11:21:52 -0300

    add: home page to render all available drinks

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,34 @@
+import api from "../../services/api";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+
+    const [drinks, setDrinks] = useState([])
+
+    useEffect(()=> {
+        api.get()
+            .then(res => {
+                const arrayOfDrinks = res.data;
+                setDrinks(arrayOfDrinks);
+                console.log(arrayOfDrinks);
+            })
+            .catch(err => console.log(err));
+    }, [])
+    
+
     return (
-        <div>Home</div>
+        <div>
+            {drinks.map((item, index)=> {
+                return (
+                    <div key={index}>
+                        <h4>{item.name}</h4>
+                        <img src={item.image_url} alt={item.name} style={{height: '100px'}} />
+                        <p>Fabricação: {item.first_brewed}</p>
+                        <p>Litros: {item.volume.value}L</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
